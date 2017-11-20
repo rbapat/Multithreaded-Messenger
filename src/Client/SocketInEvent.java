efac545bfe728fda9b6438dddd5df5f9d48412ec
rbapat <rbapat@users.noreply.github.com>
2017-11-20 10:03:42 -0500

    Update SocketInEvent.java

--- src/Client/SocketInEvent.java
+++ src/Client/SocketInEvent.java
@@ -4,16 +4,36 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.Observable;
 
+/*
+ * Event Handler that runs as its own thread and notifies observers when it recieved input from a given socket
+ * 
+ * @author Rohan Bapat
+ * @version 1.0
+ * @since 06-15-17
+ */
+
 public class SocketInEvent extends Observable implements Runnable {
 
 	BufferedReader socketIn;
 	private boolean isAlive;
-
+	
+	/*
+	 * instantiate input stream to socket
+	 *
+	 * @param socketIn
+	 *	BufferedReader to socket
+	 *
+	 */
 	public SocketInEvent(BufferedReader socketIn) {
 		this.socketIn = socketIn;
 		isAlive = true;
 	}
-
+	
+	/*
+	 * main loop for thread
+	 * notifies registered observers when it recieves input from a given socket
+	 *
+	 */
 	public void run() {
 		while (isAlive) {
 			try {
@@ -28,6 +48,9 @@ public class SocketInEvent extends Observable implements Runnable {
 		}
 	}
 	
+	/*
+	 * kills thread when called
+	 */
 	public void kill() {
 		isAlive = false;
 	}
