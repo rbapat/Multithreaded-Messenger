9d56068cebd2d97b5dd75aeb94b450febae7b638
rbapat <rbapat@users.noreply.github.com>
2017-11-20 10:05:57 -0500

    Update StdInEvent.java

--- src/Client/StdInEvent.java
+++ src/Client/StdInEvent.java
@@ -4,15 +4,36 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.Observable;
 
+/*
+ * Event Handler that runs as its own thread and notifies observers when it recieved input from STDIN
+ * 
+ * @author Rohan Bapat
+ * @version 1.0
+ * @since 06-15-17
+ */
+
 public class StdInEvent extends Observable implements Runnable {
 	BufferedReader stdIn;
 	private boolean isAlive;
+	
+	/*
+	 * instantiate input stream to STDIN
+	 *
+	 * @param stdIn
+	 *	BufferedReader to STDIN
+	 *
+	 */
 	
 	public StdInEvent(BufferedReader stdIn) {
 		this.stdIn = stdIn;
 		isAlive = true;
 	}
-
+	
+	/*
+	 * main loop for thread
+	 * notifies registered observers when it recieves input from a given socket
+	 *
+	 */
 	public void run() {
 		while (isAlive) {
 			try {
@@ -27,6 +48,9 @@ public class StdInEvent extends Observable implements Runnable {
 		}
 	}
 	
+	/*
+	 * kills thread when called
+	 */
 	public void kill() {
 		isAlive = false;
 	}
